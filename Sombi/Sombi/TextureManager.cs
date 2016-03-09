1fce8f7a25f64521cc568f3a99e8a9e27e24a659
Dentzing <dentzing@hotmail.com>
2016-03-09 16:11:00 +0100

    TextureManager

---
+++ Sombi/Sombi/TextureManager.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sombi
+{
+    class TextureManager
+    {
+        public static Texture2D tileTex { get; private set; }
+
+        public static void LoadContent(ContentManager Content)
+        {
+            tileTex = Content.Load<Texture2D>("tile");
+        }
+    }
+}
