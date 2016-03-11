df8c504cca138953df077469803cbed8485a8601
PatrikFlodin <dTj69xX4>
2016-03-11 11:21:13 +0100

    Revert "removed one texturelibrary class"
    This reverts commit b5f5f6b259a8b28cc4833fa9189292a4c5b0f942.

---
+++ Sombi/Sombi/Library/TextureLibrary.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sombi
+{
+    public class TextureLibrary
+    {
+        public static Texture2D tileTex { get; private set; }
+        public Texture2D testMapTex;
+        public static Texture2D player1Tex { get; private set; }
+        public static Texture2D player2Tex { get; private set; }
+
+        public void LoadContent(ContentManager Content)
+        {
+            tileTex = Content.Load<Texture2D>("tile");
+            testMapTex = Content.Load<Texture2D>("TestMap");
+        }
+    }
+}
