64a811d3790a7e5bdded69a7c35fe69008a6c0d5
Dentzing <dentzing@hotmail.com>
2016-03-11 11:44:40 +0100

    Added constructor to GameObject

--- Sombi/Sombi/GameObject.cs
+++ Sombi/Sombi/GameObject.cs
@@ -9,16 +9,20 @@ namespace Sombi
 {
     class GameObject
     {
+        Texture2D tex;
+        Vector2 pos;
 
-
-
-
-
+        public GameObject(Texture2D tex, Vector2 pos)
+        {
+            this.tex = tex;
+            this.pos = pos;
+        }
 
         public void Update(GameTime gameTime)
         {
 
         }
+
         public void Draw(SpriteBatch spriteBatch)
         {
 
