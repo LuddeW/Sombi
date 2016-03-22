003c9328f4e475663722e9a995b9420a06710441
xzorcious <simonrosengren123@gmail.com>
2016-03-22 11:18:53 +0100

    Started to implementing the Zombie class

--- Sombi/Sombi/Zombie.cs
+++ Sombi/Sombi/Zombie.cs
@@ -9,14 +9,30 @@ namespace Sombi
 {
     class Zombie
     {
-
+        float health;
+        Vector2 pos;
+        Vector2 direction;
+        float velocity;
+        int activationRange;
+        public Zombie(Vector2 startPos)
+        {
+            this.velocity = 100;
+            this.pos = startPos;
+            this.direction = new Vector2(0, 0);
+            this.health = 70;
+            this.activationRange = 250;
+        }
         public void Update(GameTime gameTime)
         {
-
+            pos += direction * velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        }
+        public void handleBulletHit(float damage)
+        {
+            this.health -= damage;
         }
         public void Draw(SpriteBatch spriteBatch)
         {
-
+            
         }
     }
 }
