45775bd78e65a05255341c33e6d7d454578b36a8
m0lin <martin.molin@live.se>
2016-03-22 11:18:16 +0100

    Added textures
    Added texture for player2 & zombie sprite sheet

--- Sombi/Sombi/Library/TextureLibrary.cs
+++ Sombi/Sombi/Library/TextureLibrary.cs
@@ -13,13 +13,15 @@ namespace Sombi
         public Texture2D testMapTex;
         public static Texture2D player1Tex { get; private set; }
         public static Texture2D player2Tex { get; private set; }
+        public static Texture2D zombieTex { get; private set; }
 
         public void LoadContent(ContentManager Content)
         {
             //tileTex = Content.Load<Texture2D>("tile");
             testMapTex = Content.Load<Texture2D>("TestMap2");
             player1Tex = Content.Load<Texture2D>(@"Player1");
-            player2Tex = Content.Load<Texture2D>(@"Player1");
+            player2Tex = Content.Load<Texture2D>(@"Player2");
+            zombieTex = Content.Load<Texture2D>(@"GreenZombie1");
         }
     }
 }
