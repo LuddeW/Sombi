d17f19442793ae34483b47ce03112acc1b9ab17a
LuddeW <ludwig.wiktorsson@gmail.com>
2016-03-22 13:52:35 +0100

    Creat the grid in the grid class instead

--- Sombi/Sombi/Grid.cs
+++ Sombi/Sombi/Grid.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -10,24 +11,49 @@ namespace Sombi
     public class Grid
     {
 
-        Tile[,] grid = new Tile[20, 20];
+        Tile[,] grid;
         Texture2D tex;
 
         public Grid()
         {
-            //CreateGrid();
+            CreateGridFactory();
         }
 
-        //public void CreateGrid()
-        //{
-        //    for (int i = 0; i < 20; i++)
-        //    {
-        //        for (int k = 0; k < 20; k++)
-        //        {
-        //            grid[i, k] = new Tile(new Vector2(i, k));
-        //        } 
-        //    }
-        //}
+        private void CreateTileMatrix(char objectChar)
+        {
+            int boardSize = 20;
+            grid = new Tile[boardSize, boardSize];
+            for (int i = 0; i < grid.GetLength(0); i++)
+            {
+                for (int k = 0; k < grid.GetLength(1); k++)
+                {
+                    switch (objectChar)
+                    {
+                        case '0':
+                            grid[i, k] = new Tile(new Vector2(i, k), true);
+                            break;
+                        case '1':
+                            grid[i, k] = new Tile(new Vector2(i, k), false);
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void CreateGridFactory()
+        {
+            StreamReader sr = new StreamReader(@"Testmap.txt");
+            int row = 0;
+            while (!sr.EndOfStream)
+            {
+                string objectStr = sr.ReadLine();
+                for (int col = 0; col < objectStr.Length; col++)
+                {
+                    CreateTileMatrix(objectStr[col]);
+                }
+                row++;
+            }
+        }
 
         public void Draw(SpriteBatch sb)
         {
--- Sombi/Sombi/Manager/GameManager.cs
+++ Sombi/Sombi/Manager/GameManager.cs
@@ -18,16 +18,17 @@ namespace Sombi
         Vector2 testMapPos;
         // TextureLibrary textureLibrary;
         Tile[,] tiles;
+        Grid grid;
 
 
         public GameManager(ContentManager contentManager)
         {
             this.contentManager = contentManager;
+            grid = new Grid();
             playerManager = new PlayerManager();
             enemyManager = new EnemyManager();
             TextureLibrary.LoadContent(contentManager);
             testMapPos = Vector2.Zero;
-            CreateGridFactory();
             enemyManager.AddZombie(new Vector2(50, 50));  //Endast för TEST!!
         }
 
@@ -42,42 +43,6 @@ namespace Sombi
             spriteBatch.Draw(TextureLibrary.testMapTex, testMapPos, Color.White);
             playerManager.Draw(spriteBatch);
             enemyManager.Draw(spriteBatch);
-        }
-
-        private void CreateTileMatrix(char objectChar)
-        {
-            int boardSize = 20;
-            tiles = new Tile[boardSize, boardSize];
-            for (int i = 0; i < tiles.GetLength(0); i++)
-            {
-                for (int k = 0; k < tiles.GetLength(1); k++)
-                {
-                    switch (objectChar)
-                    {
-                        case '0':
-                            tiles[i, k] = new Tile(new Vector2(i, k), true);
-                            break;
-                        case '1':
-                            tiles[i, k] = new Tile(new Vector2(i, k), false);
-                            break;
-                    }                  
-                }
-            }
-        }
-
-        private void CreateGridFactory()
-        {
-            StreamReader sr = new StreamReader(@"Testmap.txt");
-            int row = 0;
-            while (!sr.EndOfStream)
-            {
-                string objectStr = sr.ReadLine();
-                for (int col = 0; col < objectStr.Length; col++)
-                {
-                    CreateTileMatrix(objectStr[col]);
-                }
-                row++;
-            }
         }
     }
 }
