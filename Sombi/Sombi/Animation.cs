58f19ec97b304e11b1d7564b6a8f698edca12d83
Dentzing <dentzing@hotmail.com>
2016-03-23 15:35:23 +0100

    Added animation

--- Sombi/Sombi/Animation.cs
+++ Sombi/Sombi/Animation.cs
@@ -1,1 +1,49 @@
-﻿
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sombi
+{
+    class Animation
+    {
+        Texture2D texture;
+        public Texture2D Texture
+        {
+            get { return texture; } // returna bild
+        }
+
+        public int frameWidth; // Bredd
+
+        public int frameHeight // Höjd
+        {
+            get { return texture.Height; } // frameHeight blir samma sak som höjden av bilden
+        }
+
+        float frameTime; // avgör hur snabbt bilden byter under animationen
+        public float FrameTime // ju högre nummret är desto slöare kmr den byta bild
+        {
+            get { return frameTime; }
+        }
+
+        public int frameCount;
+
+        bool isLooping; // en bool som bestämmer om animationen loopar eller inte
+        public bool IsLooping
+        {
+            get { return isLooping; }
+        }
+
+        public Animation(Texture2D newTexture, int newFrameWidth, float newFrameTime, bool newIsLooping)
+        {
+            // vi har bild, bredden, hastigheten vid animationens förändring av bilder, och om den loopar eller ej.
+            texture = newTexture;
+            frameWidth = newFrameWidth;
+            frameTime = newFrameTime;
+            isLooping = newIsLooping;
+            frameCount = texture.Width / frameWidth;
+
+        }
+    }
+}
