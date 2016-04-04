975f60efc29c3f6b511d5de972f33079cbcd0304
PatrikFlodin <dTj69xX4>
2016-04-04 12:31:03 +0200

    Changed hud text

--- Sombi/Sombi/Manager/EnemyManager.cs
+++ Sombi/Sombi/Manager/EnemyManager.cs
@@ -63,7 +63,7 @@ namespace Sombi
             {
                 z.Draw(spriteBatch);
             }
-            spriteBatch.DrawString(TextureLibrary.HUDText, "Number of Zombies killed: " + zombies.Count, new Vector2(450, 15), Color.Black);
+            spriteBatch.DrawString(TextureLibrary.HUDText, "Number of Zombies: " + zombies.Count, new Vector2(450, 15), Color.Black);
 
         }
         public void CheckForBulletCollisions(List<Bullet> bulletList)
