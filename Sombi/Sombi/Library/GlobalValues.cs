4c073b6acd3895fd2d583536c339155ef9d9109c
m0lin <martin.molin@live.se>
2016-04-21 10:05:43 +0200

    temporary Testmap

--- Sombi/Sombi/Library/GlobalValues.cs
+++ Sombi/Sombi/Library/GlobalValues.cs
@@ -9,10 +9,10 @@ namespace Sombi
     public class GlobalValues
     {
         public const int TILE_SIZE = 50;
-        public static Vector2 GRID_SIZE = new Vector2(76, 40);
+        public static Vector2 GRID_SIZE = new Vector2(76, 60);
 
         public static Random rnd = new Random();
-        public static Rectangle windowBounds = new Rectangle(0, 0, 3800, 2000);
+        public static Rectangle windowBounds = new Rectangle(0, 0, 3800, 3000);
         public static Vector2 cameraBounds = new Vector2(1900, 1000);
         public static Vector2 screenBounds = new Vector2(1900,1000);
         
