61d2fbb332316f58360d1de3b7cab6c786f7ea34
xzorcious <simonrosengren123@gmail.com>
2016-04-28 12:26:59 +0200

    weaponWheel for player 1 working

--- Sombi/Sombi/Manager/HUDManager.cs
+++ Sombi/Sombi/Manager/HUDManager.cs
@@ -27,6 +27,7 @@ namespace Sombi
         {
             hudPos.X = cameraPos.X;
             hudPos.Y = cameraPos.Y;
+            WeaponRotation();
 
         }
         public void WeaponRotation()
@@ -63,7 +64,7 @@ namespace Sombi
         public void Draw(SpriteBatch spriteBatch, int numberOfPlayers)
         {
             spriteBatch.Draw(TextureLibrary.player1ScoreHud, new Vector2(hudPos.X + 0, hudPos.Y + 0) , Color.White);          
-            spriteBatch.Draw(TextureLibrary.weaponHud, new Vector2(hudPos.X + 0, hudPos.Y + GlobalValues.screenBounds.Y - TextureLibrary.weaponHud.Height), Color.White * 0.8f);
+            spriteBatch.Draw(TextureLibrary.weaponWheel[weaponRotationIndex], new Vector2(hudPos.X + 0, hudPos.Y + GlobalValues.screenBounds.Y - TextureLibrary.weaponHud.Height), Color.White * 0.8f);
             spriteBatch.DrawString(TextureLibrary.HudText, "Health: " + players[0].health, new Vector2(hudPos.X + 15, hudPos.Y + 10), Color.Black);
             spriteBatch.DrawString(TextureLibrary.HudText, "Cash: " + players[0].cash, new Vector2(hudPos.X + 15, hudPos.Y + 25), Color.Black);
             if (numberOfPlayers == 2)
